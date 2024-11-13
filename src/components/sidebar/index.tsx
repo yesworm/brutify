979682c8e77fcb8312724782aeea1e2edb3be754
sam geyer <geyerwork@gmail.com>
2024-11-13 23:51:46 +1100

    started working on the getTracks useEffect hook - currently in WIP, resolved some code errors and syntax issues

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -4,12 +4,58 @@ interface IProps {
     playlists: Array<string>
 }
 
-const Sidebar:React.FC<IProps> = ({ playlists }) => {
+const Sidebar:React.FC<IProps> = ({ playlists, getTracks }) => {
+
+
+    const styles = {
+        menuList: (styles) => {
+            return {
+                ...styles,
+                maxHeight: "25vh"
+            };
+        },
+        option: (styles:any, { isFocused }: { isFocused: any}) => {
+            return {
+                ...styles,
+                backgroundColor: isFocused ? "#999999" : null,
+                color: "#333333",
+            }
+        }
+    }
+
+    const handleChange = ((e:any) => {
+        console.log(e.id)
+    })
 
     return(
         <>
             <Select
                 options={playlists}
+                getOptionLabel={(e:any)=>e.name}
+                styles={styles}
+                theme={(theme) => ({
+                    ...theme,
+                    borderRadius: 0,
+                    colours: {
+                        ...theme.colors,
+                        primary: 'black',
+                        primary25: 'grey',
+                        primary50: 'grey',
+                        primary75: 'grey',
+                        neutral0: 'white',
+                        neutral5: 'white',
+                        neutral10: 'white',
+                        neutral20: 'black',
+                        neutral30: 'black',
+                        neutral40: 'white',
+                        neutral50: 'black',
+                        neutral60: 'black',
+                        neutral70: 'black',
+                        neutral80: 'white',
+                        neutral90: 'black',
+
+                    }
+                })}
             />
         </>
     )
