d270be2385701e7d499a454641286f3c24379090
sam geyer <geyerwork@gmail.com>
2024-11-20 00:17:53 +1100

    updated vercel.json file and config files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'  
-import './index.css'
 import App from './components/app'
 
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -13,5 +12,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    typescript: {
+      ignoreBuildErrors: true
+    }
   },
 })
